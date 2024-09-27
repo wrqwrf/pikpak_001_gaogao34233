c59d30bc3d6bdf056d319634606b07937fe11573
wrqwrf <gaogao34233@proton.me>
2024-09-27 20:23:20 +0800

    Create run.py

---
+++ .github/workflows/run.py
@@ -0,0 +1,13 @@
+pikpak_auto_invite 
+├── /.github/
+│  │──  /workflows/
+│  │  │── run.yml           Github Actions配置文件
+├── README.md
+├── /models/
+│  │── pikpak3.0.onnx       图像识别模型
+│  │── pikpak4.0.onnx       图像识别模型
+│  │── charsets.json        配置
+├── /temp/                  缓存验证图片
+├── run.py                  程序主入口
+├── image.py                处理图片
+└── recognize.py            图像识别
